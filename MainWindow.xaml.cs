6b8e3b56e2875a4e5065978eaaa94362cc1a0af9
Nigel <m0cvo@yahoo.co.uk>
2026-08-10 10:38:43 +0100

    added save and load buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text;
 using System.Windows;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace Names
 {
@@ -16,6 +19,7 @@ namespace Names
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> lstNames1;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,11 +32,48 @@ namespace Names
                 lstNames.Items.Add(txtName.Text);
                 txtName.Clear();
             }
+            
         }
 
         private void CloseBtn_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                DefaultExt = ".txt"
+            };
+
+            if (saveFileDialog.ShowDialog(this) == true)
+            {
+                using (Stream stream = saveFileDialog.OpenFile())
+                using (StreamWriter writer = new StreamWriter(stream, Encoding.UTF8))
+                {
+                    foreach (var item in lstNames.Items)
+                    {
+                        writer.WriteLine(item?.ToString());
+                    }
+                }
+            }
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            {
+                openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";   
+            }
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (var item in openFileDialog.FileName)
+                {// Process selected file
+                    lstNames.Items.Add(item);
+                }
+            }
         }
     }
 }
